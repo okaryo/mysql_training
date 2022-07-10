f968a3cab382115fd2cd94d24a1018ffa46ff6f1
okaryo <okaryotr@gmail.com>
2022-07-10 23:22:45 +0900

    :recycle: remove debug print

--- init/init.sh
+++ init/init.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-echo 'hoge'
-
-# DDLでテーブルを作成する
 mysql -u root -proot sakila < "/docker-entrypoint-initdb.d/sql/sakila-schema.sql"
 
-# データを流し込む
 mysql -u root -proot sakila < "/docker-entrypoint-initdb.d/sql/sakila-data.sql"
